f8fbc00ba542343d10e46ba027f6745660b4b6ed
Thiago da Paz <thiagopazjunior@gmail.com>
2022-07-04 20:28:52 -0300

    Iniciando tela inicial.

--- desafio/src/App.vue
+++ desafio/src/App.vue
@@ -1,60 +1,44 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+      <v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+      <div class="text-h2">
+            {{ slide }} Slide
+          </div>    
+        </v-row>
+      
+    </v-carousel-item>
+  </v-carousel>
+    </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
+  data() {
+   return {
+     slides: [1, 2, 3]
+   }
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
