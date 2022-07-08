e26f20b5e1b77d2583db3aa5c61544378766eecc
Thiago da Paz <thiagopazjunior@gmail.com>
2022-07-07 22:30:49 -0300

    feat: Criado funcionalidade de remover cidades listadas.

--- teste-tecnico/src/components/InitialArea/CardsCitys.vue
+++ teste-tecnico/src/components/InitialArea/CardsCitys.vue
@@ -7,6 +7,7 @@
         <h4>Máxima: {{ city.forecast[0].max }}ºC</h4>
         <h4>Minima: {{ city.forecast[0].min }}ºC</h4>
         <h4>Dia: {{ city.forecast[0].date }}</h4>
+        <button @click="removeCity(city)">Remover cidade</button>
       </li>
     </ul>
   </div>
@@ -15,6 +16,15 @@
 <script>
   export default {
     name: 'CardsCitys',
+    methods: {
+      removeCity(city) {
+        const { citysInitials } = this;
+        const findCity = citysInitials.find((el) => el.city_name == city.city_name);
+        const indexCity = citysInitials.indexOf(findCity);
+
+        citysInitials.splice(indexCity, 1);
+      }
+    },
     computed: {
       citysInitials() {
         return this.$store.state.citys;
